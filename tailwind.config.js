4a31bcbe9913999175a15db36ca267421255cb4b
Christian De La Cruz <christian2793@gmail.com>
2024-07-24 18:46:10 -0400

    restore git

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "rgba(210, 180, 140, 1)",
+        secondary: "rgba(150, 150, 150, 0.7)",
+        tertiary: "rgba(240, 240, 240, 1)",
+        heading: '#F8F4E3',
+        paragraph: '#E2DCCB',
+      },
+      fontFamily: {
+        heading: ['Philosopher', 'sans-serif'],
+        paragraph: ['Philosopher', 'sans-serif'],
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+    },
+  },
+  plugins: [],
+};
